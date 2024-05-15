493378cf10879706cf58f8a071a734c24a7d2a01
MANSI081 <115340913+MANSI081@users.noreply.github.com>
2024-05-15 17:26:22 +0530

    Time: 13 ms (26.47%), Space: 21.5 MB (95.82%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,30 +11,59 @@
  */
 class Solution {
 public:
-int getHeight(TreeNode* root){
-   if(root==NULL)return false;
-   int lh=getHeight(root->left);
-   int rh=getHeight(root->right);
+// int getHeight(TreeNode* root){
+//    if(root==NULL)return false;
+//    int lh=getHeight(root->left);
+//    int rh=getHeight(root->right);
 
-   return 1+max(lh,rh);
-}
+//    return 1+max(lh,rh);
+// }
+
+
+//     bool isBalanced(TreeNode* root) {
+
+//         // *****************approach_1  O(n2)****************
+//         if(root==NULL)return true;
+
+
+//         //here in next approach we remove these lh nad rh by which complexity become O(n)
+//         int lh=getHeight(root->left);
+//         int rh=getHeight(root->right);
+
+//         if(abs(lh-rh)>1)return false;//it should be <=
+
+//         bool left=isBalanced(root->left);
+//         bool right=isBalanced(root->right);
+
+//         if(!left || !right)return false; //if any onr give you a false return false else true
+//         return true;
+        
+//     }
+
+    // ************************************approach2 complexity O(n)************
 
 
     bool isBalanced(TreeNode* root) {
+        return dfsHeight(root)!=-1;///redturn -1 then the balanced binary tree nahi tojh unbalnaced binary tree
+        //  Check if the tree's height difference
+        // between subtrees is less than 2
+        // If not, return false; otherwise, return true
 
-        // *****************approach_1  O(n2)****************
-        if(root==NULL)return true;
+    }
+int dfsHeight(TreeNode* root){
+       if(root==NULL)return 0;
+       int lh=dfsHeight(root->left);
+       
+       if(lh==-1 )return -1;
+       int rh=dfsHeight(root->right);
+       
+       if(rh==-1)return -1;
 
-        int lh=getHeight(root->left);
-        int rh=getHeight(root->right);
+   
 
-        if(abs(lh-rh)>1)return false;//it should be <=
+       if(abs(lh-rh)>1)return -1;
 
-        bool left=isBalanced(root->left);
-        bool right=isBalanced(root->right);
-
-        if(!left || !right)return false; //if any onr give you a false return false else true
-        return true;
-        
+       return max(lh,rh)+1;
+       
     }
 };

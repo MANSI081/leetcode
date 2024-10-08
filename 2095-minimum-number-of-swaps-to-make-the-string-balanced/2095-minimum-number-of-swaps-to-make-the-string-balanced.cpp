a3b7d0d43722ca65a3c4323d348780029824fbea
MANSI081 <115340913+MANSI081@users.noreply.github.com>
2024-10-09 00:39:00 +0530

    Time: 110 ms (45.97%), Space: 35.2 MB (41.42%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
+
+// /BALNCED IF AND ONLY IF 
+// BEFORE BALANCED==> ]][]][[[][  ===> JO BALNCED HAI UNKO IGNORE KAR DO
+// AFTER BALANCED===> ]]][[[ ==>ISI FORMAT ME HOGA HAMESH 
+// AND IN QUESTION GIVEN EQUAL NUMBER OF OPENING AND EQUAL NUMBER OF CLOSING
+// THOUGHT 1
+// (NUMBER OF OPENING BRACKET+1)/2
+
+// STACK IS THE BEST APPROACH FOR PARENTHESIS REATED PROBLEM
+// AGAR OPEN BRACKET HAI TABHI STACK ME DALNA HAI
+// NHI TO CLOSE BRACKET HAI CHECK KAR LO STACK KE TOP PE KOI OPEN BRACKET HAI , AGR HAI TOH POP KAR DO
+
+
+// 
     int minSwaps(string s) {
-        int size = 0;
-        int n = s.size();
-        for (int i = 0; i < n; i++) {
-            char ch = s[i];
-            if (ch == '[')
-                size++;
-            else if (size > 0) 
-                size--;
+
+    // with stack
+        stack<char>st;
+
+        for(char &ch: s){
+            if(ch=='['){
+                st.push(ch);
+            } else if(!st.empty()){
+                st.pop(); //balancing closing bracket with an open bracket in stack
+
+            }
         }
-        return (size + 1) / 2;
+        // size of stack= number of open barckets in the stack
+        return (st.size()+1)/2;
     }
 };
